f30600f623c3afcb013a9b065c2cb940035e8a47
Andrew <58674899+iBrushC@users.noreply.github.com>
2020-11-02 17:06:28 -0500

    Added __init__.py

---
+++ __init__.py
@@ -0,0 +1,34 @@
+##################
+## Initiation
+##################
+
+bl_info = {
+    "name": "AnimExtras",
+    "author": "Andrew Combs",
+    "version": (0, 0, 1),
+    "blender": (2, 90, 0),
+    "description": "True onion skinning",
+    "category": "Animation"
+}
+
+import bpy
+from .ons.gui import *
+from .ons.ops import *
+
+classes = [ANMX_gui, ANMX_data, ANMX_set_onion, ANMX_draw_meshes, ANMX_clear_onion, ANMX_update_onion]
+
+
+def register():
+    for c in classes:
+        bpy.utils.register_class(c)
+    
+    bpy.types.Scene.anmx_data = bpy.props.PointerProperty(type=ANMX_data)
+    
+    
+def unregister():
+    for c in classes:
+        bpy.utils.unregister_class(c)
+    
+
+if __name__ == "__main__":
+    register()
