c414445b20cc429c2e8110f2bd9a314c1f646edb
Andrew <58674899+iBrushC@users.noreply.github.com>
2020-12-12 16:54:17 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@
 bl_info = {
     "name": "AnimExtras",
     "author": "Andrew Combs",
-    "version": (1, 0, 3),
+    "version": (1, 1, 0),
     "blender": (2, 80, 0),
     "description": "True onion skinning",
     "category": "Animation"
@@ -15,16 +15,18 @@ import bpy
 from .ons.gui import *
 from .ons.ops import *
 
+# All classes that need to be initialized
 classes = [ANMX_gui, ANMX_data, ANMX_set_onion, ANMX_draw_meshes, ANMX_clear_onion, ANMX_update_onion]
 
-
+# Register function
 def register():
     for c in classes:
         bpy.utils.register_class(c)
     
+	# External pointer property definitions
     bpy.types.Scene.anmx_data = bpy.props.PointerProperty(type=ANMX_data)
     
-    
+# Unregister function
 def unregister():
     for c in classes:
         bpy.utils.unregister_class(c)
