1ffdf031c1ced3578606bb6974e7c33b31c3b676
Andrew <58674899+iBrushC@users.noreply.github.com>
2020-12-12 16:53:52 -0500

    Update gui.py

--- ons/gui.py
+++ ons/gui.py
@@ -5,21 +5,26 @@ import bpy
 from .ops import *
 
 
-''' GUI '''
 class ANMX_gui(bpy.types.Panel):
-    """Panel for all MotionFX Operations"""
+    """Panel for all onion skinning Operations"""
     bl_idname = 'VIEW3D_PT_animextras_panel'
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
     bl_category = 'AnimExtras'
     bl_label = 'Onion Skinning'
 
-
+    
+    # All functions for drawing the panel go in here
+    # TODO:
+    # -- Seperate different areas (which control toggleable areas) into seperate functions
+    
     def draw(self, context):
+        # All predefined variables
         layout = self.layout
         access = context.scene.anmx_data
         obj = context.object
         
+        # Makes sure the user can't do any operations when the onion object doesn't exist
         if access.onion_object not in bpy.data.objects:
             layout.operator("anim_extras.set_onion")
             return
@@ -28,35 +33,36 @@ class ANMX_gui(bpy.types.Panel):
             row.operator("anim_extras.update_onion", text="Update")
             row.operator("anim_extras.clear_onion", text="Clear Selected")
         
+        # Top label for the onion object. This may be replaced with a list if support for multiple onion objects is added
         col = layout.column()
         col.label(text="Current Onion Object: {}".format(access.onion_object))
         
+        # Dropdown menu for the onion mode
         col.prop(access, "onion_mode")
-        
         col.separator(factor=1)
         
-
+        # Area for changing the amount of onion skins
         row = layout.row()
-    
         row = row.split(factor=0.3)
-        
         row.label(text="Amount:")
         row.prop(access, "skin_count", text="")
         
+        # Special exception for if the mode is Per-Frame-Stepped
+        # TODO:
+        # -- Make this better. This isn't a futureproof method, and should probably be expandable
         if access.onion_mode == "PFS":
             row = layout.row()
-    
             row = row.split(factor=0.3)
-            
             row.label(text="Step:")
             row.prop(access, "skin_step", text="")
         
-        row = layout.row(align=True)
+        # Boxes for selecting the past and future onion skinning options
         
-        text = "Past"
+        text = "Past"  # A text variable is used so that the box labels can be changed (different modes require different settings)
         if access.onion_mode == "INB":
             text = "Inbetween Color"
-        
+        # Past box
+        row = layout.row(align=True)
         box = row.box()
         col = box.column(align=True)
         col.label(text=text)
@@ -65,11 +71,11 @@ class ANMX_gui(bpy.types.Panel):
         col.prop(access, "past_opacity_end", text="End Opacity", slider=True)
         col.prop(access, "past_enabled")
         
-        text = "Future"
         
+        text = "Future"  # See text = "Past"
         if access.onion_mode == "INB":
             text = "Direct Keying Color"
-        
+        # Future box
         box = row.box()
         col = box.column(align=True)
         col.label(text=text)
@@ -80,11 +86,13 @@ class ANMX_gui(bpy.types.Panel):
         
         layout.separator(factor=1)
         
+        # Settings for xray and flat shading
         layout.prop(access, "use_xray")
         layout.prop(access, "use_flat")
         
         layout.separator(factor=1)
         
+        # Drawing toggle button., variable is used to display status
         text = "Draw"
         if access.toggle:
             text = "Stop Drawing"
