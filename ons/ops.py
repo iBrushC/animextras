87da38b878e57fa956666ff2ffea7cccc2f91cd2
Andrew <58674899+iBrushC@users.noreply.github.com>
2020-12-12 16:53:22 -0500

    Update ops.py

--- ons/ops.py
+++ ons/ops.py
@@ -10,40 +10,34 @@ from gpu_extras.batch import batch_for_shader
 import numpy as np
 from mathutils import Vector, Matrix
 
-# ########################################################## #
-# Data (stroring it in the object or scene doesnt work well) #
-# ########################################################## #
+# External data
+# Can't be stored in scene or object data because dictionaries aren't supported
 shader = gpu.shader.from_builtin('3D_UNIFORM_COLOR')
 frame_data = dict([])
 batches = dict([])
 extern_data = dict([])
 
-# ################ #
-# Functions        #
-# ################ #
+# Functions
 def frame_get_set(_obj, frame):
+	# Gets all of the data from a mesh on a certain frame
     tmpobj = _obj
 
     # Setting the frame to get an accurate reading of the object on the selected frame
     scn = bpy.context.scene
     scn.frame_set(frame)
 
-    # Getting the Depenency Graph and the evaluated object
+    # Applying the Depenency Graph (applying all modifiers) and getting the mesh
     depsgraph = bpy.context.evaluated_depsgraph_get()
     eval = tmpobj.evaluated_get(depsgraph)
-    
-    # Making a new mesh from the object.
     mesh = eval.to_mesh()
     mesh.update()
     
-    # Getting the object's world matrix
+    # Transforming the mesh by its world matrix (does the equivalent of applying transforms) so that the meshes can all be in world space.
     mat = Matrix(_obj.matrix_world)
-    
-    # This moves the mesh by the object's world matrix, thus making everything global space. This is much faster than getting each vertex individually and doing a matrix multiplication on it
     mesh.transform(mat)
     mesh.update()
     
-    # loop triangles are needed to properly draw the mesh on screen
+    # loop triangles are needed to properly draw the mesh and its faces on screen
     mesh.calc_loop_triangles()
     mesh.update()
     
@@ -52,6 +46,7 @@ def frame_get_set(_obj, frame):
     indices = np.empty((len(mesh.loop_triangles), 3), 'i')
     
     # Getting all of the vertices and incices all at once (from: https://docs.blender.org/api/current/gpu.html#mesh-with-random-vertex-colors)
+    # There is no way that this script would be possible without this
     mesh.vertices.foreach_get(
         "co", np.reshape(vertices, len(mesh.vertices) * 3))
     mesh.loop_triangles.foreach_get(
@@ -67,10 +62,9 @@ def set_to_active(_obj):
     
     scn = bpy.context.scene
     
+    # Clears the onion object, sets it to active and sets the frames and batches
     clear_active()
-    
     scn.anmx_data.onion_object = _obj.name
-
     bake_frames()
     make_batches()
 
@@ -90,14 +84,16 @@ def clear_active():
 
 
 def make_batches():
-    # Custom OSL shader could be set here
+    # Creates all the batches needed for drawing. All frames are calculated, then only certain ones are drawn.
     scn = bpy.context.scene
     access = scn.anmx_data
     _obj = bpy.data.objects[access.onion_object]
     
-    
+    # For every key(frame) in the calculated frame data it creates a batch with a shader that can be drawn in a single call.
+    # TODO:
+    # -- See if its possible to combine all frames into a single batch
     for key in frame_data:
-        arg = frame_data[key]
+        arg = frame_data[key]  # Dictionaries are used rather than lists or arrays so that frame numbers are a given
         vertices = arg[0]
         indices = arg[1]
         batch = batch_for_shader(shader, 'TRIS', {"pos": vertices}, indices=indices)
@@ -105,16 +101,20 @@ def make_batches():
         
 
 def bake_frames():
-    # Needs to do the following:
-    # 1. Bake the data for every frame and store it in the objects "["frame_data"]" items
+    # Goes through every single (necessary) frame and gets the object's data
     scn = bpy.context.scene
     access = scn.anmx_data
     _obj = bpy.data.objects[access.onion_object]
     
+    # Reset value
     curr = scn.frame_current
     step = access.skin_step
     
-    # Getting the first and last frame of the animation
+    # Getting the first and last frame that there is animation data on either the object or its parent object
+    # Makes sure that there are no unnecessary frames that are calculated and speeds up the script
+    # TODO:
+    # -- Fix random bugs with odd numbers
+    # -- Allow for a custom keyobj (linked rigs do not work)
     keyobj = _obj
     
     if _obj.parent is not None:
@@ -126,29 +126,35 @@ def bake_frames():
             keyframes.append(int(k.co[0]))
             
     keyframes = np.unique(keyframes)
-    
     start = int(np.min(keyframes))
     end = int(np.max(keyframes)) + 1
     
+    # Different modes need to collect frames in different ways, which is done here
+    # TODO:
+    # -- Make this more streamlined and expandable, maybe using a scene EnumProperty
     if access.onion_mode == "PF":
+        # Per-Frame
         for f in range(start, end):
             arg = frame_get_set(_obj, f)
             frame_data[str(f)] = arg
         extern_data.clear()
         
     elif access.onion_mode == "PFS":
+        # Per-Frame-Stepped
         for f in range(start, end, step):
             arg = frame_get_set(_obj, f)
             frame_data[str(f)] = arg
         extern_data.clear()
         
     elif access.onion_mode == "DC":
+        # Direct Keys
         for fkey in keyframes:
             arg = frame_get_set(_obj, fkey)
             frame_data[str(fkey)] = arg
         extern_data.clear()
         
     elif access.onion_mode == "INB":
+        # Inbetweening
         for f in range(start, end):
             arg = frame_get_set(_obj, f)
             frame_data[str(f)] = arg
@@ -161,18 +167,15 @@ def bake_frames():
     scn.frame_set(curr)
     
 
-# ################ #
-# Properties       #
-# ################ #
-
-
+# Properties
 class ANMX_data(PropertyGroup):
     # Custom update function for the toggle
     def toggle_update(self, context):
         if self.toggle:
             bpy.ops.anim_extras.draw_meshes('INVOKE_DEFAULT')
         return
-        
+    
+    # All the different modes are stored here
     modes = [("PF", "Per-Frame", "", 1), ("PFS", "Per-Frame Stepped", "", 2), ("DC", "Direct Keys", "", 3), ("INB", "Inbetweening", "", 4)]
 
     # Onion Skinning Properties
@@ -196,23 +199,26 @@ class ANMX_data(PropertyGroup):
     future_opacity_end: bpy.props.FloatProperty(name="Ending Opacity", min=0, max=1, default=0.1)
     future_enabled: bpy.props.BoolProperty(name="Enabled?", default=True)
 
-  
-# ################ #
-# Operators        #
-# ################ #
 
-
+# Operators
+# Most of these are just shells for existing functions, so optimizations are definitely possible
+# P.S. "ANMX" is used as a prefix for any operators used in animation extras. This will probably change to "OS" when PathTools is added
 class ANMX_set_onion(Operator):
+    # Operator for setting the onion object
     bl_idname = "anim_extras.set_onion"
     bl_label = "Set Onion To Selected"    
     bl_description = "Sets the selected object to be the onion object"
     
     def execute(self, context):
         obj = context.active_object
-    
+        
+        # Tons of error checks to make sure the script can't mess itself up
         if obj == None:
             return {"CANCELLED"}
         
+        # Checks to make sure the object actually has animation
+        # TODO:
+        # -- Find a way to get rid of AttributeError try/except functions
         if obj.parent is None:
             try:
                 obj.animation_data.action.fcurves
@@ -220,10 +226,12 @@ class ANMX_set_onion(Operator):
                     return {"CANCELLED"}
         else:
             try:
+                # This right here needs to change for allowing linked rigs
                 obj.parent.animation_data.action.fcurves
             except AttributeError:
                     return {"CANCELLED"}
         
+        # Makes sure the object is actually a mesh
         if obj.type == 'MESH':
             set_to_active(obj)
     
@@ -231,26 +239,25 @@ class ANMX_set_onion(Operator):
 
 
 class ANMX_clear_onion(Operator):
+    # Operator for clearing the onion object
+    # This seems unnecessary
     bl_idname = "anim_extras.clear_onion"
     bl_label = "Clear Selected Onion"
     bl_description = "Clears the path of the onion object"
-    
+   
     def execute(self, context):
         clear_active()
-    
         return {"FINISHED"}
     
     
 class ANMX_update_onion(Operator):
+    # Operator for updating the onion object
     bl_idname = "anim_extras.update_onion"
     bl_label = "Update Selected Onion"
     bl_description = "Updates the path of the onion object"
     
     def execute(self, context):
-        if context.active_object is None:
-            return {"FINISHED"}
-        
-        if context.active_object.type == 'MESH':
+        if context.scene.anmx_data.onion_object in bpy.data.objects:
             set_to_active(bpy.data.objects[context.scene.anmx_data.onion_object])
     
         return {"FINISHED"}
@@ -258,6 +265,7 @@ class ANMX_update_onion(Operator):
 # Uses a list formatted in the following way to draw the meshes:
 # [[vertices, indices, colors], [vertices, indices, colors]]
 class ANMX_draw_meshes(Operator):
+    # Operator for actually drawing the meshes. Most of this was learned from https://www.youtube.com/user/jayanamgames
     bl_idname = "anim_extras.draw_meshes"
     bl_label = "Draw"
     bl_description = "Draws a set of meshes without creating objects"
@@ -267,67 +275,74 @@ class ANMX_draw_meshes(Operator):
         self.handler = None
         self.timer = None
         self.mode = None
-
+    
+    # What happens when the operation is first invoked. Addes the modal handler and registers it with a timer
     def invoke(self, context, event):
         self.register_handlers(context)
         context.window_manager.modal_handler_add(self)
         self.mode = context.scene.anmx_data.onion_mode
         return {'RUNNING_MODAL'}
-        
+    # For some reason this needs a timer
     def register_handlers(self, context):
         self.timer = context.window_manager.event_timer_add(0.1, window=context.window)
         self.handler = bpy.types.SpaceView3D.draw_handler_add(self.draw_callback, (context,), 'WINDOW', 'POST_VIEW')
-        
+    # gets rid of the draw handler and timer
     def unregister_handlers(self, context):
         context.scene.anmx_data.toggle = False
         context.window_manager.event_timer_remove(self.timer)
         if self.handler != None:
             bpy.types.SpaceView3D.draw_handler_remove(self.handler, 'WINDOW')
         self.handler = None
-    
+    # Runs in the background to allow the operator to be stopped
     def modal(self, context, event):
         if context.scene.anmx_data.onion_object not in bpy.data.objects:
             self.unregister_handlers(context)
             return {'CANCELLED'}
-        
         if context.scene.anmx_data.toggle is False or self.mode != context.scene.anmx_data.onion_mode:
             self.unregister_handlers(context)
             return {'CANCELLED'}
         
         return {'PASS_THROUGH'}
     
+    # Gets rid of the handlers when finished.
     def finish(self, context):
         self.unregister_handlers(context)
         return {'FINISHED'}
-        
+    
+    # The main drawing code
     def draw_callback(self, context):
+        # Drawing callback that determines how everything should be drawn
         scn = context.scene
         ac = scn.anmx_data
         f = scn.frame_current
-        
         pc = ac.past_color
         fc = ac.future_color
         
-        
-        
         override = False
-        
         color = (0, 0, 0, 0)
-        
         threshold = ac.skin_count
         
+        # Doesnt draw if viewport overlays are off
         if context.space_data.overlay.show_overlays == False:
             return
         
+        # Draws each batch inside the batches, the "key" variable is the frame that that batch is on
+        # TODO:
+        # -- Make it not a complete mess
+        # -- Simplify color sampling
+        # -- Try to reduce the number of if/else statements
         for key in batches:
             f_dif = abs(f-int(key))
             
-            # Getting the color if the batch is in the past
-            
+            # The extern data determines how the colors are drawn. If the length isn't zero, inbetweening is active; otherwise, normal color sampling is active
+            # TODO:
+            # -- See if this can be done through a shader. (Note: would require custom GL batches to store which frame the mesh was stored on)
             if len(extern_data) == 0:
+                # Standard color sampling
+                # Getting the color if the batch is in the past
                 if f > int(key):
                     if ac.past_enabled:
-                        color = (pc[0], pc[1], pc[2], ac.past_opacity_start-((ac.past_opacity_start-ac.past_opacity_end)/ac.skin_count) * f_dif)
+                        color = (pc[0], pc[1], pc[2], ac.past_opacity_start-((ac.past_opacity_start-ac.past_opacity_end)/ac.skin_count) * f_dif)  # This is horrible
                     else:
                         override = True
                 # Getting the color if the batch is in the future
@@ -337,17 +352,20 @@ class ANMX_draw_meshes(Operator):
                     else:
                         override = True
             else:
+                # Inbetweening color sampling
+                # If the key is a direct key it is drawn with the future color, otherwise its drawn with the past color
                 if key in extern_data:
                     color = (fc[0], fc[1], fc[2], ac.future_opacity_start-((ac.future_opacity_start-ac.future_opacity_end)/ac.skin_count) * f_dif)
                 else:
                     color = (pc[0], pc[1], pc[2], ac.past_opacity_start-((ac.past_opacity_start-ac.past_opacity_end)/ac.skin_count) * f_dif)
             
-            # Only draws if the frame is not the current one, it is within the skin limits, and there has not been an override
+            # Only draws if the frame is not the current one, it is within the skin limits, and there has not been an override. This could definitely be done with a shader.
             if f != int(key) and f_dif <= ac.skin_count and not override:
+                # Shader is finally bound using the defined color
                 shader.bind()
                 shader.uniform_float("color", color)
                 
-                # Theres gotta be a better way to do this. Seems super inefficient
+                # This seems inefficient, but nothing works properly without it.
                 if not ac.use_flat:
                     bgl.glEnable(bgl.GL_BLEND)
                     bgl.glEnable(bgl.GL_CULL_FACE)
@@ -359,5 +377,6 @@ class ANMX_draw_meshes(Operator):
                 bgl.glDisable(bgl.GL_BLEND)
                 bgl.glDisable(bgl.GL_CULL_FACE)
                 bgl.glDisable(bgl.GL_DEPTH_TEST)
-                
+            
+            # Resets the override
             override = False
